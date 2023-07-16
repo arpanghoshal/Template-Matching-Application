09d52938b346132d94678d7e533ef968a04c9757
Arpan Ghoshal <arpanghoshalwork@gmail.com>
2023-07-16 13:19:48 -0400

    Update pseudo_algorithm_training.py

--- pseudo_algorithm_training.py
+++ pseudo_algorithm_training.py
@@ -11,20 +11,20 @@ Begin Retraining Process
     - Divide 'processed_data' into 'training_set', 'validation_set', and 'testing_set'
 
 3. Model Training:
-    - Load pre-trained GPT model 'gpt_model'
-    - Configure 'gpt_model' for training (optimizer, loss function, metrics)
+    - Load pre-trained LLM model 'LLM_model'
+    - Configure 'LLM_model' for training (optimizer, loss function, metrics)
     - For each 'epoch' up to 'max_epochs':
-        - Train 'gpt_model' on 'training_set', validate on 'validation_set'
-        - Save 'gpt_model' weights at regular intervals
+        - Train 'LLM_model' on 'training_set', validate on 'validation_set'
+        - Save 'LLM_model' weights at regular intervals
         - If validation performance doesn't improve over a specified number of 'epochs', stop training early 
 
 4. Model Evaluation:
-    - Evaluate 'gpt_model' on 'testing_set'
+    - Evaluate 'LLM_model' on 'testing_set'
     - If model performance is satisfactory, proceed to update the model; otherwise, revisit training or data preprocessing steps
 
 5. Updating the Model:
     - While the application is in use:
         - Collect new labeled data from successful transformations
-        - Periodically retrain 'gpt_model' on the updated dataset
+        - Periodically retrain 'LLM_model' on the updated dataset
 
 End Retraining Process
