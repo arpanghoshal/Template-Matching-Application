62b000245c938fe454adbd7a1af8111bad26dda2
Arpan Ghoshal <arpanghoshalwork@gmail.com>
2023-07-17 09:46:52 -0400

    Update pseudo_algorithm_fewshot.py

--- pseudo_algorithm_fewshot.py
+++ pseudo_algorithm_fewshot.py
@@ -5,10 +5,10 @@ Begin Few-shot Learning Process
 2. Whenever a successful transformation logic is generated:
     - Add the 'transformation_logic' to a temporary list 'new_examples'
 
-3. Sort 'new_examples' based on distinctiveness or complexity of transformation logic
+3. Sort 'new_examples' based on distinctiveness or complexity of transformation logic (MMR Algorithm)
 
 4. If the length of 'prompt_examples' is less than 5:
-    - Add the most distinctive examples from 'new_examples' to 'prompt_examples' until it contains 5 examples (MMR Algorithm)
+    - Add the most distinctive examples from 'new_examples' to 'prompt_examples' until it contains 5 examples 
 Else:
     - Compare the new examples with the existing examples in 'prompt_examples'
     - If a new example is more distinctive or complex than an existing example, replace the least distinctive example in 'prompt_examples' with the new one
